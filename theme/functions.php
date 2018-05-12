d83d434dcac32bbd36d9e4179ad428a98336dd41
Benfarhat <benfarhat.elyes@gmail.com>
2018-05-12 19:39:11 +0100

    addng scripts for better accessibility

--- theme/functions.php
+++ theme/functions.php
@@ -132,7 +132,10 @@ function hfsi_scripts() {
   /* Scripts */
 	wp_enqueue_script( 'jquery', get_theme_file_uri( '/js/jquery.min.js' ), false, '3.3.1', true );
 	wp_enqueue_script( 'popper', get_theme_file_uri( '/js/popper.min.js' ), false, '3.3.1', true );
-	wp_enqueue_script( 'bootstrap', get_theme_file_uri( '/js/bootstrap.min.js' ), false, '3.3.1', true );
+  wp_enqueue_script( 'bootstrap', get_theme_file_uri( '/js/bootstrap.min.js' ), false, '3.3.1', true );
+  /* For better user experience */
+  wp_enqueue_script( 'hfsi-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'hfsi-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
