fbe62dfb6195f5e2fbba1ea2223659e4ecbdab3f
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-05 12:14:25 +0100

    Better rendering for button and informations on card-footer

--- theme/archive.php
+++ theme/archive.php
@@ -73,12 +73,12 @@ get_header();
             <p class="card-text"><?= the_excerpt(); ?></p>
           </div>
           <div class="card-footer">
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
+            <div class="row d-flex justify-content-between align-items-center">
+              <div class="btn-group col-lg-5">
                 <a href="<?= get_permalink( get_the_ID() )?>" class="btn btn-sm btn-outline-success">Consulter <i class="fa fa-angle-right"></i></a>
                 <?php edit_post_link( '<i class="fa fa-edit"></i>', '', '', null, 'btn btn-sm btn-outline-secondary btn-edit-post-link' ); ?>
               </div>
-              <small class="text-muted"><i class="fa fa-eye"></i> <?= hfsi_getPostViews(get_the_ID()) ?> <?= hfsi_getPostViews(get_the_ID()) > 1 ? "vues" : "vue" ?>&nbsp;|&nbsp;<i class="fa fa-clock-o"></i> <?php the_date( 'd/m/Y', null, null, true ); ?></small>
+              <small class="col-lg-7 text-muted d-flex justify-content-end"><span><i class="fa fa-eye"></i> <?= hfsi_getPostViews(get_the_ID()) ?> <?= hfsi_getPostViews(get_the_ID()) > 1 ? "vues" : "vue" ?>&nbsp;|&nbsp;<i class="fa fa-clock-o"></i> <?php the_date( 'd/m/Y', null, null, true ); ?></span></small>
             </div>
           </div>
         </div>
