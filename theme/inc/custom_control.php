e6e2ae4f5a1b44be730e9f384e55e582d8932daa
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-10 17:13:36 +0100

    clone and modifying repeater custom control to a repeater custom control group

---
+++ theme/inc/custom_control.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * Skyrocket Customizer Custom Controls
+ * We only need the sortable repeater custom control
+ *
+ */
+
+if ( class_exists( 'WP_Customize_Control' ) ) {
+
+
+	/**
+	 * Original: Sortable Repeater Custom Control
+	 *
+	 * @author Anthony Hortin <http://maddisondesigns.com>
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @link https://github.com/maddisondesigns
+   *
+   * We need this custom control to control our webservice form
+   * Modified to add more input element like fieldname field type and field label
+	 */
+	class Skyrocket_Sortable_Repeater_Custom_Control extends WP_Customize_Control {
+		/**
+		 * The type of control being rendered
+		 */
+		public $type = 'sortable_repeater';
+		/**
+ 		 * Button labels
+ 		 */
+		public $button_labels = array();
+		/**
+		 * Constructor
+		 */
+		public function __construct( $manager, $id, $args = array(), $options = array() ) {
+			parent::__construct( $manager, $id, $args );
+			// Merge the passed button labels with our default labels
+			$this->button_labels = wp_parse_args( $this->button_labels,
+				array(
+					'add' => __( 'Add', 'ephemeris' ),
+				)
+			);
+		}
+		/**
+		 * Enqueue our scripts and styles
+		 */
+		public function enqueue() {
+			wp_enqueue_script( 'skyrocket_custom_controls_js', trailingslashit( get_template_directory_uri() ) . 'js/customizer.js', array( 'jquery', 'jquery-ui-core', 'jquery-ui-sortable' ), '1.0', true );
+			wp_enqueue_style( 'skyrocket_custom_controls_css', trailingslashit( get_template_directory_uri() ) . 'css/customizer.css', array(), '1.0', 'all' );
+		}
+		/**
+		 * Render the control in the customizer
+		 */
+		public function render_content() {
+		?>
+	      <div class="sortable_repeater_control">
+				<?php if( !empty( $this->label ) ) { ?>
+					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+				<?php } ?>
+        <?php
+        /* We don't need this, description will be used as placeholder
+        if( !empty( $this->description ) ) { ?>
+					<span class="customize-control-description"><?php echo esc_html( $this->description ); ?></span>
+        <?php }
+        */?>
+				<input type="hidden" id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $this->value() ); ?>" class="customize-control-sortable-repeater" <?php $this->link(); ?> />
+				<div class="sortable">
+          <div class="repeater" style="border:1px solid #ccc;padding:2px;background-color:#fff;margin-top:4px;">
+            <input type="text" value="" class="repeater-input" style="width:100%;border:1px solid #ddd;" placeholder="Fieldname..." />
+            <input type="text" value="" class="repeater-label-input" style="width:100%;border:1px solid #ddd;" placeholder="Label for this field..." />
+            <select class="repeater-type-input" style="width:100%;border:1px solid #ddd;" >
+              <option value="text">Text</option>
+              <option value="date">Date</option>
+              <option value="integer">Integer</option>
+            </select>
+            <input type="text" value="" class="repeater-class-input" style="width:100%;border:1px solid #ddd;" placeholder="Custom class..." />
+            <span class="dashicons dashicons-sort"></span><a class="customize-control-sortable-repeater-delete" href="#"><span class="dashicons dashicons-no-alt"></span></a>
+          </div>
+				</div>
+				<button class="button customize-control-sortable-repeater-add" type="button"><?php echo $this->button_labels['add']; ?></button>
+			</div>
+		<?php
+		}
+	}
+
+	/**
+	 * URL sanitization
+	 *
+	 * @param  string	Input to be sanitized (either a string containing a single url or multiple, separated by commas)
+	 * @return string	Sanitized input
+	 */
+	if ( ! function_exists( 'skyrocket_url_sanitization' ) ) {
+		function skyrocket_url_sanitization( $input ) {
+			if ( strpos( $input, ',' ) !== false) {
+				$input = explode( ',', $input );
+			}
+			if ( is_array( $input ) ) {
+				foreach ($input as $key => $value) {
+					$input[$key] = esc_url_raw( $value );
+				}
+				$input = implode( ',', $input );
+			}
+			else {
+				$input = esc_url_raw( $input );
+			}
+			return $input;
+		}
+	}
+
+	/**
+	 * Switch sanitization
+	 *
+	 * @param  string		Switch value
+	 * @return integer	Sanitized value
+	 */
+	if ( ! function_exists( 'skyrocket_switch_sanitization' ) ) {
+		function skyrocket_switch_sanitization( $input ) {
+			if ( true === $input ) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+
+	/**
+	 * Radio Button and Select sanitization
+	 *
+	 * @since Ephemeris 1.0
+	 *
+	 * @param  string		Radio Button value
+	 * @return integer	Sanitized value
+	 */
+	if ( ! function_exists( 'skyrocket_radio_sanitization' ) ) {
+		function skyrocket_radio_sanitization( $input, $setting ) {
+			//get the list of possible radio box or select options
+         $choices = $setting->manager->get_control( $setting->id )->choices;
+
+			if ( array_key_exists( $input, $choices ) ) {
+				return $input;
+			} else {
+				return $setting->default;
+			}
+		}
+	}
+
+	/**
+	 * Integer sanitization
+	 *
+	 * @param  string		Input value to check
+	 * @return integer	Returned integer value
+	 */
+	if ( ! function_exists( 'skyrocket_sanitize_integer' ) ) {
+		function skyrocket_sanitize_integer( $input ) {
+			return (int) $input;
+		}
+	}
+
+	/**
+	 * Text sanitization
+	 *
+	 * @param  string	Input to be sanitized (either a string containing a single string or multiple, separated by commas)
+	 * @return string	Sanitized input
+	 */
+	if ( ! function_exists( 'skyrocket_text_sanitization' ) ) {
+		function skyrocket_text_sanitization( $input ) {
+			if ( strpos( $input, ',' ) !== false) {
+				$input = explode( ',', $input );
+			}
+			if( is_array( $input ) ) {
+				foreach ( $input as $key => $value ) {
+					$input[$key] = sanitize_text_field( $value );
+				}
+				$input = implode( ',', $input );
+			}
+			else {
+				$input = sanitize_text_field( $input );
+			}
+			return $input;
+		}
+	}
+
+	/**
+	 * Array sanitization
+	 *
+	 * @param  array	Input to be sanitized
+	 * @return array	Sanitized input
+	 */
+	if ( ! function_exists( 'skyrocket_array_sanitization' ) ) {
+		function skyrocket_array_sanitization( $input ) {
+			if( is_array( $input ) ) {
+				foreach ( $input as $key => $value ) {
+					$input[$key] = sanitize_text_field( $value );
+				}
+			}
+			else {
+				$input = '';
+			}
+			return $input;
+		}
+	}
+
+	/**
+	 * Alpha Color (Hex & RGBa) sanitization
+	 *
+	 * @param  string	Input to be sanitized
+	 * @return string	Sanitized input
+	 */
+	if ( ! function_exists( 'skyrocket_hex_rgba_sanitization' ) ) {
+		function skyrocket_hex_rgba_sanitization( $input, $setting ) {
+			if ( empty( $input ) || is_array( $input ) ) {
+				return $setting->default;
+			}
+
+			if ( false === strpos( $input, 'rgba' ) ) {
+				// If string doesn't start with 'rgba' then santize as hex color
+				$input = sanitize_hex_color( $input );
+			} else {
+				// Sanitize as RGBa color
+				$input = str_replace( ' ', '', $input );
+				sscanf( $input, 'rgba(%d,%d,%d,%f)', $red, $green, $blue, $alpha );
+				$input = 'rgba(' . skyrocket_in_range( $red, 0, 255 ) . ',' . skyrocket_in_range( $green, 0, 255 ) . ',' . skyrocket_in_range( $blue, 0, 255 ) . ',' . skyrocket_in_range( $alpha, 0, 1 ) . ')';
+			}
+			return $input;
+		}
+	}
+
+	/**
+	 * Only allow values between a certain minimum & maxmium range
+	 *
+	 * @param  number	Input to be sanitized
+	 * @return number	Sanitized input
+	 */
+	if ( ! function_exists( 'skyrocket_in_range' ) ) {
+		function skyrocket_in_range( $input, $min, $max ){
+			if ( $input < $min ) {
+				$input = $min;
+			}
+			if ( $input > $max ) {
+				$input = $max;
+			}
+		    return $input;
+		}
+	}
+
+	/**
+	 * Google Font sanitization
+	 *
+	 * @param  string	JSON string to be sanitized
+	 * @return string	Sanitized input
+	 */
+	if ( ! function_exists( 'skyrocket_google_font_sanitization' ) ) {
+		function skyrocket_google_font_sanitization( $input ) {
+			$val =  json_decode( $input, true );
+			if( is_array( $val ) ) {
+				foreach ( $val as $key => $value ) {
+					$val[$key] = sanitize_text_field( $value );
+				}
+				$input = json_encode( $val );
+			}
+			else {
+				$input = json_encode( sanitize_text_field( $val ) );
+			}
+			return $input;
+		}
+	}
+
+	/**
+	 * Date Time sanitization
+	 *
+	 * @param  string	Date/Time string to be sanitized
+	 * @return string	Sanitized input
+	 */
+	if ( ! function_exists( 'skyrocket_date_time_sanitization' ) ) {
+		function skyrocket_date_time_sanitization( $input, $setting ) {
+			$datetimeformat = 'Y-m-d';
+			if ( $setting->manager->get_control( $setting->id )->include_time ) {
+				$datetimeformat = 'Y-m-d H:i:s';
+			}
+			$date = DateTime::createFromFormat( $datetimeformat, $input );
+			if ( $date === false ) {
+				$date = DateTime::createFromFormat( $datetimeformat, $setting->default );
+			}
+			return $date->format( $datetimeformat );
+		}
+  }
+
+
+}
---
+++ theme/js/customizer.js
@@ -0,0 +1,123 @@
+jQuery( document ).ready(function($) {
+	"use strict";
+
+	/**
+	 * Based on Repeater Custom Control
+	 * from Anthony Hortin <http://maddisondesigns.com>
+	 * @license http://www.gnu.org/licenses/gpl-2.0.html
+	 * @link https://github.com/maddisondesigns
+	 */
+
+	// Update the values for all our input fields and initialise the sortable repeater
+	$('.sortable_repeater_control').each(function() {
+		// If there is an existing customizer value, populate our rows
+		var defaultValuesArray = $(this).find('.customize-control-sortable-repeater').val().split(',');
+		var numRepeaterItems = defaultValuesArray.length;
+
+		if(numRepeaterItems > 0) {
+			// Add the first item to our existing input field
+			$(this).find('.repeater-input').val(defaultValuesArray[0]);
+			// Create a new row for each new value
+			if(numRepeaterItems > 1) {
+				var i;
+				for (i = 1; i < numRepeaterItems; ++i) {
+					skyrocketAppendRow($(this), defaultValuesArray[i]);
+				}
+			}
+		}
+	});
+
+	// Make our Repeater fields sortable
+	jQuery(this).find('.sortable').sortable({
+		update: function(event, ui) {
+			skyrocketGetAllInputs($(this).parent());
+		}
+	});
+
+	// Remove item starting from it's parent element
+	$('.sortable').on('click', '.customize-control-sortable-repeater-delete', function(event) {
+		event.preventDefault();
+		var numItems = $(this).parent().parent().find('.repeater').length;
+
+		if(numItems > 1) {
+			$(this).parent().slideUp('fast', function() {
+				var parentContainer = $(this).parent().parent();
+				$(this).remove();
+				skyrocketGetAllInputs(parentContainer);
+			})
+		}
+		else {
+			$(this).parent().find('.repeater-input').val('');
+			skyrocketGetAllInputs($(this).parent().parent().parent());
+		}
+	});
+
+	// Add new item
+	$('.customize-control-sortable-repeater-add').click(function(event) {
+		event.preventDefault();
+		skyrocketAppendRow($(this).parent());
+		skyrocketGetAllInputs($(this).parent());
+	});
+
+	// Refresh our hidden field if any fields change
+	$('.sortable').change(function() {
+		skyrocketGetAllInputs($(this).parent());
+	})
+
+	// Add https:// to the start of the URL if it doesn't have it
+	$('.sortable').on('blur', '.repeater-input', function() {
+		var field = $(this);
+		var val = field.val();
+		if(val && !val.match(/^.+:\/\/.*/)) {
+			// Important! Make sure to trigger change event so Customizer knows it has to save the field
+			field.val(val).trigger('change');
+		}
+	});
+
+	// Append a new row to our list of elements
+	function skyrocketAppendRow($element, defaultValue = '') {
+		//var newRow = '<div class="repeater" style="display:none"><input type="text" value="' + defaultValue + '" class="repeater-input" placeholder="https://" /><span class="dashicons dashicons-sort"></span><a class="customize-control-sortable-repeater-delete" href="#"><span class="dashicons dashicons-no-alt"></span></a></div>';
+		var newRow = '<div class="repeater" style="border:1px solid #ccc;padding:2px;background-color:#fff;margin-top:4px;"><input type="text" value="' + defaultValue + '" class="repeater-input" style="width:100%;border:1px solid #ddd;" placeholder="<?php echo esc_html( $this->description ); ?>" /><select class="repeater-type-input" style="width:100%;border:1px solid #ddd;" ><option value="url">URL</option><option value="text">Text</option><option value="date">Date</option><option value="integer">Integer</option></select><span class="dashicons dashicons-sort"></span><a class="customize-control-sortable-repeater-delete" href="#"><span class="dashicons dashicons-no-alt"></span></a></div>';
+		var newRow = `					<div class="repeater" style="border:1px solid #ccc;padding:2px;background-color:#fff;margin-top:4px;">
+    <input type="text" value="" class="repeater-input" style="width:100%;border:1px solid #ddd;" placeholder="Fieldname..." />
+    <input type="text" value="" class="repeater-label-input" style="width:100%;border:1px solid #ddd;" placeholder="Label for this field..." />
+    <select class="repeater-type-input" style="width:100%;border:1px solid #ddd;" >
+      <option value="url">URL</option>
+      <option value="text">Text</option>
+      <option value="date">Date</option>
+      <option value="integer">Integer</option>
+    </select>
+    <input type="text" value="" class="repeater-class-input" style="width:100%;border:1px solid #ddd;" placeholder="Custom class..." />
+    <span class="dashicons dashicons-sort"></span><a class="customize-control-sortable-repeater-delete" href="#"><span class="dashicons dashicons-no-alt"></span></a>
+  </div>`;
+
+		$element.find('.sortable').append(newRow);
+		$element.find('.sortable').find('.repeater:last').slideDown('slow', function(){
+			$(this).find('input').focus();
+		});
+	}
+
+	// Get the values from the repeater input fields and add to our hidden field
+	function skyrocketGetAllInputs($element) {
+    console.log($element);
+		var inputValues = $element.find('.repeater').map(function() {
+		//var inputValues = $element.find('.repeater-input').map(function() {
+      return {
+        fieldname: $(this).find('.repeater-input').val(),
+        label: $(this).find('.repeater-label-input').val(),
+        type: $(this).find('.repeater-type-input').val(),
+        class: $(this).find('.repeater-class-input').val(),
+      };
+			//return $(this).val();
+    }).toArray();
+    console.log(inputValues)
+		// Add all the values from our repeater fields to the hidden field (which is the one that actually gets saved)
+		$element.find('.customize-control-sortable-repeater').val(inputValues);
+		// Important! Make sure to trigger change event so Customizer knows it has to save the field
+		$element.find('.customize-control-sortable-repeater').trigger('change');
+	}
+
+
+
+
+});
