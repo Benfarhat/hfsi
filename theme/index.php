d9c57d56f6c235124659e94b242a5c7a4d3c23d8
Benfarhat <benfarhat.elyes@gmail.com>
2018-06-12 13:30:31 +0100

    enable only admin mode for test and debug

--- theme/index.php
+++ theme/index.php
@@ -20,25 +20,40 @@ if ( get_theme_mod('enable_category_carousel', true) ):
 endif;
 ?>
   <?php if ( get_theme_mod('enable_webservice', true) || get_theme_mod('enable_rendezvous', true) ): // @todo
-    // At this position we have at least on of them;
-    $taille = (get_theme_mod('enable_webservice', true) && get_theme_mod('enable_rendezvous', true)) ? 6 : 12 ;
+    // At this position we have at least one of them;
+    $can_run_webservice = true;
+    $only_admin = false;
+    // if only admin mode
+    if ( get_theme_mod('enable_admin_webservice', false)):
+      $current_user = wp_get_current_user();
+      if (user_can( $current_user, 'administrator' )) {
+        $can_run_webservice = true;
+        $only_admin = true; // For debug message @todo
+      } else {
+        $can_run_webservice = false;
+      }
+    endif;
+    $taille = (get_theme_mod('enable_webservice', true) && $can_run_webservice && get_theme_mod('enable_rendezvous', true)) ? 6 : 12 ;
     ?>
   <!-- Resultats d'analyses et rendez vous -->
   <section id="horaire-rdv">
     <div class="container mt-3">
       <div class="row">
-      <?php if ( get_theme_mod('enable_webservice') ): ?>
+
+      <?php if ( get_theme_mod('enable_webservice') && $can_run_webservice ): ?>
         <!-- Resultats d'analyses -->
         <div id="resultat-analyse" class="col-md-<?= $taille ?>">
           <?php get_template_part( 'elements/webservice_form' ); // col-md-6 ?>
         </div>
       <?php endif; ?>
+
       <?php if ( get_theme_mod('enable_rendezvous', true) ): ?>
         <!-- Rendez vous -->
         <div id="rdv" class="col-md-<?= $taille ?>">
           <?php get_template_part( 'elements/appointment' ); // col-md-6 ?>
         </div>
       <?php endif; ?>
+
       </div>
     </div>
   </section>
